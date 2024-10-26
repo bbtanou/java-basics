f28bb76b8d1b6498be9f3aef01c66986349bd393
bbtanou <bbtanousatina@gmail.com>
2024-10-26 16:09:08 +0000

    add crud to S5

--- S5/final/index.js
+++ S5/final/index.js
@@ -5,12 +5,14 @@ const port = 3000;
 
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+const config = {
     host: 'localhost',
     user: 'odc',
     password: 'odc123',
     database: 'odc'
-});
+}
+
+const db = mysql.createConnection(config);
 
 db.connect((err) => {
     if (err){
@@ -31,16 +33,39 @@ app.get("/", (request, response) => {
 })
 
 app.post('/save', (request, response) => {
-    const {nom, telephone} = request.body;
+    const {id, nom, telephone} = request.body;
+
+    if (!nom || nom === '' || !telephone || telephone === ''){
+        response.json({message: "Vous devez des donnee des valeurs pour chaque champs"});
+        return;
+    }
+
+    if (id){
+        db.query('update person set nom = ?, telephone = ? where id = ?',[nom, telephone, id], (err, result) => {
+            if (err){
+                console.log("Impossible de modifier l'enregistrement: " + id);
+                console.log(err);
+                return;
+            }
+
+            response.json({data: result});
+        })
+
+        return;
+    }
+
     db.query(
-        'insert into person (nom, telephone) values(?, ?)',
+        `insert into person (nom, telephone) values(?, ?)`,
         [nom, telephone],
         (er, result) => {
         if (er){
             console.log("Impossible d'enregistrer les donnees", er);
+            return;
         }
 
         console.log('Donnee enregistree avec succes: ', result);
+
+        response.json(result);
     })
 
 })
@@ -53,6 +78,32 @@ app.get('/list', async (request, response) => {
         }
 
         response.json({data: results});
+    })
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const param = req.params.id;
+    db.query('delete from person where id = ?', [param], (err, result) => {
+        if (err){
+            console.log("Impossible d'effectuer la suppresion");
+            return;
+        }
+
+        res.json("Suppression effectuée avec succes");
+        console.log('Suppresion de la personne avec id: ' + param + ' effectuée')
+    });
+})
+
+app.get(`/person/:id`, (req, res) => {
+    const param = req.params.id;
+
+    db.query('select * from person where id = ?', [param], (err, result) => {
+        if (err){
+            console.log('impossible de trouver la personne avec id ', param);
+            return;
+        }
+
+        res.json({data: result});
     })
 })
 
